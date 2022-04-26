8629fbcc9ab541254999b16051d98c82d883f33a
k-kasama <kasakei0633@yahoo.co.jp>
2022-04-27 00:02:10 +0900

    class based view導入

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
+class Index(TemplateView):
+    template_name = "Index.html"
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the app index.")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "Template View test index  "
+        return context
